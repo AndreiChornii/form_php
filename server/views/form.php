038758f3d5011c08b183f56a21594a3fcceb4e82
xomolya <dp131192pka@gmail.com>
2019-11-09 12:36:22 +0200

    create-php-app

---
+++ server/views/form.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/public/style.css">
+    <script src="/public/main.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <form class="auth" action="">
+            <fieldset>
+                <legend>Авторизация</legend>
+                <div class="auth__row">
+                    <label for="username">User name</label>
+                    <input class="auth__text" type="text" id="username">
+                    <i class="auth__error">Don't contain (_, -, {}, $, 0-9, length > 5 characters)</i>
+                </div>
+                <div class="auth__row">
+                    <label for="userpass">User pass</label>
+                    <input class="auth__text" type="password" id="userpass">
+                </div>
+                <div class="auth__row">
+                    <label for="useremail">User email</label>
+                    <input class="auth__text" type="text" id="useremail">
+                </div>
+                <label class="auth__check">
+                    Subscribe
+                    <input type="checkbox" id="usersubscribe">
+                </label>
+                <div class="auth__row">
+                    <button class="auth__btn">Login</button>
+                </div>
+            </fieldset>
+        </form>
+    </main>
+</body>
+</html>
