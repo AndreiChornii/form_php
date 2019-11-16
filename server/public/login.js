a99a84d23292bff12cfdfd56a4880bad76dda434
xomolya <dp131192pka@gmail.com>
2019-11-16 12:52:57 +0200

    regexp

--- server/public/login.js
+++ server/public/login.js
@@ -3,9 +3,15 @@ var userName = document.querySelector("#username");
 var userPass = document.querySelector("#userpass");
 var userEmail = document.querySelector("#useremail");
 var userSubscribe = document.querySelector("#usersubscribe");
+
 var sendBtn = document.querySelector("#sendbtn");
 
 function send() {
+
+    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/gi.test(userName.value))){
+        alert('Not valid name [а-яa-z0-9\-_\.]{2,25}');
+        return false;
+    }
 
     var data = {
         name: userName.value,
