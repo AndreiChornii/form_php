a7081a0b87705ae81de6736c80cbb358aa49f7ae
xomolya <dp131192pka@gmail.com>
2019-11-19 21:42:45 +0200

    validation_form

--- server/public/login.js
+++ server/public/login.js
@@ -1,17 +1,43 @@
 'use strict'
 var userName = document.querySelector("#username");
-var userPass = document.querySelector("#userpass");
+var userNameError = document.querySelector("#username + .auth__error");
+
 var userEmail = document.querySelector("#useremail");
+var userEmailError = document.querySelector("#useremail + .auth__error");
+
+var userPhone = document.querySelector("#userphone");
+var userPhoneError = document.querySelector("#userphone + .auth__error");
+
+var userPass = document.querySelector("#userpass");
+var userPassError = document.querySelector("#userpass + .auth__error");
+
 var userSubscribe = document.querySelector("#usersubscribe");
 
 var sendBtn = document.querySelector("#sendbtn");
 
 function send() {
 
-    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/gi.test(userName.value))){
-        alert('Not valid name [а-яa-z0-9\-_\.]{2,25}');
-        return false;
+    var isError = false;
+
+    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))){
+        userNameError.classList.remove('auth__error_hide');
+        userNameError.classList.add('auth__error_show');
+        isError = true;
     }
+
+    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z.\-_]{1,10}$/i.test(userEmail.value))) {
+        userEmailError.classList.remove('auth__error_hide');
+        userEmailError.classList.add('auth__error_show');
+        isError = true;
+    }
+
+    if (!(/^(\+380|380|80}0){1,4}[0-9]{9}$/i.test(userPhone.value))) {
+        userPhoneError.classList.remove('auth__error_hide');
+        userPhoneError.classList.add('auth__error_show');
+        isError = true;
+    }
+
+    if (isError) return null;
 
     var data = {
         name: userName.value,
@@ -25,6 +51,22 @@ function send() {
         body: JSON.stringify(data)
     });
 }
+
+function setClearHandler(){
+    var elements = document.querySelectorAll('.auth__text');
+
+    elements.forEach(function(element) {
+        element.onclick = function(){
+            console.log(this.nextElementSibling);
+            this.nextElementSibling.classList.remove('auth__error_show');
+            this.nextElementSibling.classList.add('auth__error_hide');
+        }
+    })
+
+    // console.log(elements);
+}
+
+setClearHandler();
 
 sendBtn.onclick = send;
 
