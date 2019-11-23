0ea0edbe4b5175b65da0d6d21c0674a36b8ff5e0
xomolya <dp131192pka@gmail.com>
2019-11-23 12:41:53 +0200

    validation_over

--- server/public/login.js
+++ server/public/login.js
@@ -1,55 +1,61 @@
 'use strict'
-var userName = document.querySelector("#username");
-var userNameError = document.querySelector("#username + .auth__error");
 
-var userEmail = document.querySelector("#useremail");
-var userEmailError = document.querySelector("#useremail + .auth__error");
+//sendBtn.onclick = send;
 
-var userPhone = document.querySelector("#userphone");
-var userPhoneError = document.querySelector("#userphone + .auth__error");
+var formManager = {
+    name: document.querySelector("#username"),
+    nameError: document.querySelector("#username + .auth__error"),
+    email: document.querySelector("#useremail"),
+    emailError: document.querySelector("#useremail + .auth__error"),
+    phone: document.querySelector("#userphone"),
+    phoneError: document.querySelector("#userphone + .auth__error"),
+    password: document.querySelector("#userpass"),
+    passwordError: document.querySelector("#userpass + .auth__error"),
+    subscribe: document.querySelector("#usersubscribe"),
+    sendBtn: document.querySelector("#sendbtn"),
+}
 
-var userPass = document.querySelector("#userpass");
-var userPassError = document.querySelector("#userpass + .auth__error");
-
-var userSubscribe = document.querySelector("#usersubscribe");
-
-var sendBtn = document.querySelector("#sendbtn");
-
-function send() {
+formManager.valid = function valid(){
 
     var isError = false;
 
-    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userName.value))){
-        userNameError.classList.remove('auth__error_hide');
-        userNameError.classList.add('auth__error_show');
+    if (!(/^[а-яa-z0-9\-_\.]{5,25}$/i.test(this.name.value))) {
+        this.nameError.classList.remove('auth__error_hide');
+        this.nameError.classList.add('auth__error_show');
         isError = true;
     }
 
-    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z.\-_]{1,10}$/i.test(userEmail.value))) {
-        userEmailError.classList.remove('auth__error_hide');
-        userEmailError.classList.add('auth__error_show');
+    if (!(/^[0-9a-z.\-_]{1,15}@[0-9a-z.\-_]{1,14}\.[a-z.\-_]{1,10}$/i.test(this.email.value))) {
+        this.emailError.classList.remove('auth__error_hide');
+        this.emailError.classList.add('auth__error_show');
         isError = true;
     }
 
-    if (!(/^(\+380|380|80}0){1,4}[0-9]{9}$/i.test(userPhone.value))) {
-        userPhoneError.classList.remove('auth__error_hide');
-        userPhoneError.classList.add('auth__error_show');
+    if (!(/^(\+380|380|80}0){1,4}[0-9]{9}$/i.test(this.phone.value))) {
+        this.phoneError.classList.remove('auth__error_hide');
+        this.phoneError.classList.add('auth__error_show');
         isError = true;
     }
 
-    if (!(/^[а-яa-z0-9\-_\.]{2,25}$/i.test(userPass.value))) {
-        userPassError.classList.remove('auth__error_hide');
-        userPassError.classList.add('auth__error_show');
+    if (!(/^[а-яa-z0-9\-_\.]{5,25}$/i.test(this.password.value))) {
+        this.passwordError.classList.remove('auth__error_hide');
+        this.passwordError.classList.add('auth__error_show');
         isError = true;
     }
 
-    if (isError) return null;
+    return !isError;
+}
+
+formManager.send = function send() {
+
+    if (!this.valid()) return null;
 
     var data = {
-        name: userName.value,
-        pass: userPass.value,
-        email: userEmail.value,
-        subscribe: userSubscribe.value,
+        name: this.name.value,
+        email: this.email.value,
+        phone: this.phone.value,
+        password: this.password.value,
+        subscribe: this.subscribe.checked,
     }
 
     fetch('/login', {
@@ -58,7 +64,7 @@ function send() {
     });
 }
 
-function setClearHandler(){
+formManager.setClearHandler = function setClearHandler(){
     var elements = document.querySelectorAll('.auth__text');
 
     elements.forEach(function(element) {
@@ -67,12 +73,12 @@ function setClearHandler(){
             this.nextElementSibling.classList.remove('auth__error_show');
             this.nextElementSibling.classList.add('auth__error_hide');
         }
-    })
-
-    // console.log(elements);
+    });
 }
 
-setClearHandler();
+formManager.init = function(){
+    this.sendBtn.onclick = this.send.bind(this);// bind чтоб в this всегда было formManager
+    this.setClearHandler();
+}
 
-sendBtn.onclick = send;
-
+formManager.init();
